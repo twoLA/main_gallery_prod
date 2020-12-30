5f47e424ad10049bd5d6bcb8d9157ef9ae6a6c17
Adam Sims <adam.sims82@gmail.com>
2020-12-30 12:21:15 -0800

    Serve loader io file from server.

--- SDC_server/index.js
+++ SDC_server/index.js
@@ -8,6 +8,8 @@ require('newrelic');
 const app = express();
 const PORT = 3030;
 
+app.use(express.static('loaderio-0387c8a47aed375e9c28d733aff2f107.txt'));
+
 app.get('/main_gallery/:id', controllers.fetchListing);
 
 app.listen(PORT, () => {
