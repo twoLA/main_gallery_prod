dc563056383f2603a4ce05db7c484c5c0a395e0f
Adam Sims <adam.sims82@gmail.com>
2020-12-30 12:56:48 -0800

    Changed loader io express.static path.

--- SDC_server/index.js
+++ SDC_server/index.js
@@ -8,7 +8,7 @@ require('newrelic');
 const app = express();
 const PORT = 3030;
 
-app.use('/loaderio-0387c8a47aed375e9c28d733aff2f107', express.static('main_gallery_prod/loaderio-0387c8a47aed375e9c28d733aff2f107.txt'));
+app.use('/loaderio-0387c8a47aed375e9c28d733aff2f107.txt', express.static('main_gallery_prod/loaderio-0387c8a47aed375e9c28d733aff2f107.txt'));
 
 app.get('/main_gallery/:id', controllers.fetchListing);
 
